0f6ff52c762ac58b498cb042f9e3f6d6f7dbbce9
meet-mobilefirst <meetprajapati.mobilefirst@gmail.com>
2021-02-05 22:16:26 +0530

    Initial Project Setup

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const bodyparser = require("body-parser");
+
+const mongoose = require("mongoose");
+
+const app = express();
+
+//middleware for body parser
+app.use(bodyparser.urlencoded({ extended: false })); //for encoding url and parsing it into json
+app.use(bodyparser.json()); //as we have to send our data into json we will use this
+
+
+//mongoDB configuration
+const db = require("./server/myurl").mongoUrl;
+
+//Attempt to connect to DB
+mongoose
+  .connect(db, { useUnifiedTopology: true, useNewUrlParser: true })
+  .then(() => console.log("Mongo DB Connected successfully"))
+  .catch((err) => console.log(`Failed to connect to Server with error ${err}`));
+
+
+//testing - route
+app.get("/", (req, res) => {
+    res.send("PCARE");
+  });
+
+
+
+const port = process.env.port || 3000;
+
+app.listen(port, () => console.log(`App is running at port ${port}`));
---
+++ server/myurl.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoUrl : "mongodb+srv://pcareadmin:pcare123@cluster0.uftal.mongodb.net/pcare?retryWrites=true&w=majority",
+    secret: "mysecret"
+}
