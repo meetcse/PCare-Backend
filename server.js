69cb11ec942fdf2fd8505e4be7cbfd5be3dfe9cf
meetcse <ymba.meet@gmail.com>
2021-03-14 17:25:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -54,6 +54,6 @@ app.use("/api/appointment", appointment);
 //treatment
 app.use("/api/treatment", treatment);
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`App is running at port ${port}`));
